ed18f69f5a0fee8fac7ebfed7dfc59157760a407
Ernane Ferreira <ernane.junior25@gmail.com>
2024-07-06 01:14:38 -0300

    feat: improve code

--- canny/main.cpp
+++ canny/main.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
-#include "opencv2/opencv.hpp"
-
-int top_slider = 10;
-int top_slider_max = 200;
-
-char TrackbarName[50];
+#include <opencv2/opencv.hpp>
 
 cv::Mat image, border;
 
-void on_trackbar_canny(int, void*){
-  cv::Canny(image, border, top_slider, 3*top_slider);
+char TrackbarName[50];
+int top_slider = 10, top_slider_max = 200;
+
+void onTrackbarCanny(int, void *)
+{
+  cv::Canny(image, border, top_slider, 3 * top_slider);
   cv::imshow("Canny", border);
 }
 
-int main(int argc, char**argv){
-  image= cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
-  
-  sprintf( TrackbarName, "Threshold inferior", top_slider_max );
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  cv::namedWindow("Canny",1);
-  cv::createTrackbar( TrackbarName, "Canny",
-                &top_slider,
-                top_slider_max,
-                on_trackbar_canny );
+  image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  if (image.empty())
+  {
+    std::cerr << "Error opening image: " << argv[1] << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  on_trackbar_canny(top_slider, 0 );
+  sprintf(TrackbarName, "Lower Threshold");
+
+  cv::namedWindow("Canny", 1);
+  cv::createTrackbar(TrackbarName, "Canny", &top_slider, top_slider_max, onTrackbarCanny);
+
+  onTrackbarCanny(top_slider, 0);
 
   cv::waitKey();
   cv::imwrite("cannyborders.png", border);
-  return 0;
+  return EXIT_SUCCESS;
 }
